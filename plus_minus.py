000fc9ca9c86b142556f1365021d2794adff419e
Hienerd93 <60064910+hienerd93@users.noreply.github.com>
2024-10-16 08:31:43 +0700

    Create plus_minus.py

---
+++ plus_minus.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+    zenoAmount = 0
+    positiveAmount = 0
+    for x in arr:
+        if x == 0:
+            zenoAmount += 1
+        if x > 0:
+            positiveAmount += 1
+    lengArr = len(arr)
+    print(positiveAmount/lengArr)
+    print((lengArr - positiveAmount - zenoAmount)/lengArr)
+    print(zenoAmount/lengArr)
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
