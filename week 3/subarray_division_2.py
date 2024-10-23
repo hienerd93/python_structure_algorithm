8fd38e81cc9a182f7adaad9e33d4ca40ea2bbba6
Hienerd93 <60064910+hienerd93@users.noreply.github.com>
2024-10-23 09:37:10 +0700

    Create subarray_division_2.py

---
+++ week 3/subarray_division_2.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import os
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # Write your code here
+    count = 0
+    iterator = range(len(s) - m + 1) if len(s) > m else range(1)
+    for index in iterator:
+        print(index)
+        sum_range = sum(s[index:index + m])
+        if sum_range == d:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
